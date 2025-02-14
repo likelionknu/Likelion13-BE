1b4ccf766f2063c94827bd27125f27a3bbd4af12
chajihyun <gh8526@hanmail.net>
2025-02-15 01:47:03 +0900

    🎨 add WebConfig
    WebConfig 추가

---
+++ src/main/java/com/springboot/jwt/login/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.springboot.jwt.login.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://likelion13-fe.vercel.app"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
